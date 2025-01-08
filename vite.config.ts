797470b7c6967688d48a0d55f1e083408f748c79
Iliasnolsson <58471856+Iliasnolsson@users.noreply.github.com>
2025-01-08 15:32:51 +0100

    - Improvement to file naming & bundled version of package

--- vite.config.ts
+++ vite.config.ts
@@ -63,7 +63,7 @@ export default defineConfig({
      
         lib: {
             entry,
-            "name": "OctamapAlpineLibrary",
+            "name": "OctamapAlpineRouter",
             formats: ["iife"],
             fileName: () => "index.js"
         },
